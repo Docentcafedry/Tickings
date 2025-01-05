4403a9cb5ae5ffd3d8f24a2894252ba807ac6ee0
Lesha <Your Email>
2025-01-05 18:06:57 +0300

    fixed client/api/build file and added domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export function buildClient(context) {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://ticeting-app.art/",
       headers: context.headers,
     });
   } else {
